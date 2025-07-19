bffe726b01c1a45ea2cd0023c366d23aacdedf3f
PanZhiQing <panzhiqing15@gmail.com>
2025-07-19 11:53:45 +0800

    更新主题配置，添加导航链接，删除不再使用的 API 示例和 Markdown 扩展示例文档

--- docs/.vitepress/themeConfig.ts
+++ docs/.vitepress/themeConfig.ts
@@ -1,10 +1,15 @@
 export const themeConfig = {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+  // https://vitepress.dev/reference/default-theme-config
+  nav: [
+    { text: 'Home', link: 'https://globalevdata.github.io' },
+    {
+      text: 'About',
+      items: [
+        { text: 'Contact Us', link: 'https://globalevdata.github.io/contact.html' },
+        { text: 'Statistics', link: 'https://globalevdata.github.io/report.html' },
+        { text: 'Subscribe', link: 'https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAYrjOiZUNFdHUDFRQ0hZSFJLRTY5VEJKTE1GVllTOS4u' }
+      ]
+    },
+    {text: 'Map', link: '/'},
+  ]
+}
